6deb155a49baa59bfb4eb8630cd635ccf786a16f
Florian Gérard <kingdomflo16@gmail.com>
2019-11-05 20:34:57 +0100

    change env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ import { env } from '../../.env';
 export const environment = {
   production: true,
   mock: false,
-  apiUrl: 'http://localhost/M-A-R-X_Backend/public/',
-  jwtTokenSign: 'iliketrain',
+  apiUrl: env.apiUrlProd,
+  jwtTokenSign: env.jwtTokenSign,
   clientID: env.clientID,
   domain: env.domain,
   packageIdentifier: env.packageIdentifier,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,10 +6,8 @@ import { env } from '../../.env';
 export const environment = {
   production: false,
   mock: false,
-  // apiUrl: 'http://localhost/M-A-R-X_Backend_PHP/public/',
-  // apiUrl: 'http://10.0.2.2:80/M-A-R-X_Backend_PHP/public/',
-  apiUrl: 'https://marx.gerardweb.eu/public/',
-  jwtTokenSign: 'iliketrain',
+  apiUrl: env.apiUrlDev,
+  jwtTokenSign: env.jwtTokenSign,
   clientID: env.clientID,
   domain: env.domain,
   packageIdentifier: env.packageIdentifier,
